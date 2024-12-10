c58aea583afa5028e11b1c01e1de26f770b3aab2
nbietry <nbietry@gmail.com>
2024-12-10 20:19:35 +0100

    Day 9 - 1 stars - python

--- Python/Day9/day9.py
+++ Python/Day9/day9.py
@@ -1,12 +1,30 @@
 INPUT_EXAMPLE = """
 2333133121414131402
 """
+def get_block_lists(disk_map):
+    free_blocks = []
+    file_blocks = []
+    for i in range(len(disk_map)):
+        if i % 2 == 0: # even indices are file blocks and odd are free blocks
+            file_blocks.append(int(disk_map[i]))
+        else:
+            free_blocks.append(int(disk_map[i]))
+    return file_blocks, free_blocks
+
+def create_blocks(file_blocks, free_blocks):
+    block_list = []
+    for i in range(len(file_blocks)):
+        block_list.append([str(i)] * file_blocks[i])
+        if i < len(free_blocks):  # free_blocks list has 1 less element than file_blocks
+            block_list.append(["."] * free_blocks[i])
+    return [item for sublist in block_list for item in sublist]
+
 
 def format_blocks(data: list):
     """
-    >>> format_blocks(list("0..111....22222"))
+    >>> ''.join(format_blocks(list("0..111....22222")))
     '022111222......'
-    >>> format_blocks(list("00...111...2...333.44.5555.6666.777.888899"))
+    >>> ''.join(format_blocks(list("00...111...2...333.44.5555.6666.777.888899")))
     '0099811188827773336446555566..............'
     """
     num_counter = len([char for char in data if char.isdigit()])
@@ -16,6 +34,18 @@ def format_blocks(data: list):
         if char == '.': char = non_dot_characters.pop(0)
         if i >= num_counter: char = '.'
         output.append(char)
-    return ''.join(output)
+    return output
 
-print(f"Part1: {format_blocks(list(open('input').read().strip()))}")
+def checkSum(block_list):
+    result = 0
+    for i, block in enumerate(block_list):
+        if block != ".":
+            result += i * int(block)
+    return result
+
+with open('input', 'r') as file:
+    disk_map = file.read()
+    disk_map = disk_map.rstrip('\n')
+
+file_blocks, free_blocks = get_block_lists(disk_map)
+print(f"Part1: {checkSum(format_blocks(create_blocks(file_blocks, free_blocks)))}")
