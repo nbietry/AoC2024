b9c5744c393bbbbaaa13758139bcc6babe50104a
nbietry <nbietry@gmail.com>
2024-12-10 18:43:28 +0100

    Day 5 - 2 stars - python

--- Python/Day5/day5.py
+++ Python/Day5/day5.py
@@ -15,8 +15,27 @@ def valide_rules(pages):
             if (y,x) in rules: return 0
     return pages[len(pages)//2]
 
+def invalid_rules(pages):
+    invalid_pages = set()
+    for page in pages:
+        for i, x in enumerate(page):
+            for y in page[i+1:]:
+                if (y,x) in rules: invalid_pages.add(tuple(page))
+                break
+    return invalid_pages
+
+def get_middle_page(pages):
+    pages_count = []
+    for i, x in enumerate(pages):
+        pages_before, pages_after = 0, 0
+        for y in pages:
+            if (y,x) in rules:
+                pages_before += 1
+            if (x,y) in rules:
+                pages_after += 1
+        pages_count.append((x, pages_before, pages_after))
+    return next(item[0] for item in pages_count if item[1] == item[2])
+
 rules, updates = parse_input()
 print(f"Part1: {sum(valide_rules(pages) for pages in updates)}")
-
-def sort_update(pages):
-    pass
+print(f"Part2: {sum(get_middle_page(pages) for pages in invalid_rules(updates))}")
